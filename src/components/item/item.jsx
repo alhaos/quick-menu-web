94722abb25ef4defdce5db3129e5d6f909f8700e
alhaos <alhaos@gmail.com>
2025-05-05 09:46:07 +0500

    commit

--- src/components/item/item.jsx
+++ src/components/item/item.jsx
@@ -1,12 +1,13 @@
 import React from 'react';
 import styles from './item.module.css';
+import image from './tmp.jpg';
 
 function Item({ caption, price }) {
   return (
     <div className={styles.box}>
       <p className={styles.caption}>{caption}</p>
       <p className={styles.price}>{price}</p>
-      <img src="D:\repo\quick-menu-web\src\assets\img\tmp.jpg" alt="on image found" />
+      <img src={image} alt="on image found" className={styles.image} />
     </div>
   );
 }
